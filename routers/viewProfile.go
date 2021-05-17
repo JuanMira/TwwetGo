2e00fd21c0c77b42e93b036c00b1fcf6ee743a95
JuanMira <juanmira20@hotmail.com>
2021-05-17 14:37:36 -0500

    Add view profile http response

---
+++ routers/viewProfile.go
@@ -0,0 +1,30 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/JuanMira/tweetgo/bd"
+)
+
+//Show profile data
+func showProfile(w http.ResponseWriter, r http.Request) {
+	id := r.URL.Query().Get("id")
+
+	if len(id) < 1 {
+		http.Error(w, "id should be send", http.StatusBadRequest)
+		return
+	}
+
+	profile, err := bd.FindProfile(id)
+
+	if err != nil {
+		http.Error(w, "An error ocurred trying find the profile "+err.Error(), 400)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(profile)
+
+}
