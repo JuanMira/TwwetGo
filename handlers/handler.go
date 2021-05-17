cfc8a20a8eaf74e24bc26512c79853892c6177ad
JuanMira <juanmira20@hotmail.com>
2021-05-17 14:39:02 -0500

    add viewProfile endpoint

--- handlers/handler.go
+++ handlers/handler.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 	"os"
 
-	middleware "github.com/JuanMira/tweetgo/middlewares"
+	mw "github.com/JuanMira/tweetgo/middlewares"
 	"github.com/JuanMira/tweetgo/routers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
+//mw means middleware
+
 //Handler port and server listening
 func Handlers() {
 	router := mux.NewRouter()
-	router.HandleFunc("/registro", middleware.CheckBD(routers.Registro)).Methods("POST")
+	router.HandleFunc("/registro", mw.CheckBD(routers.Registro)).Methods("POST")
+	router.HandleFunc("/login", mw.CheckBD(routers.Login)).Methods("POST")
+	router.HandleFunc("/viewProfile", mw.CheckBD(mw.ValidateJWT(routers.ViewProfile))).Methods("GEt")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
